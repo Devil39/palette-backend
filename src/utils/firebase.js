6d6312cac4aa995aa893675bb86bc6ff1b0e3c69
ZiyanK <ziyankarmali786@gmail.com>
2020-05-12 20:37:13 +0530

    Modified firebase file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,7 +2,6 @@ const admin = require('firebase-admin');
 const dotenv = require("dotenv");
 dotenv.config();
 
-var serviceAccount = require(process.env.CREDS)
 admin.initializeApp({
     credential: admin.credential.cert({
         "type": process.env.FIREBASE_TYPE,
