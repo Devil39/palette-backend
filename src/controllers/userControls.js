1db14ce599a2a2454a2a38a88f9a2d266013ac07
ZiyanK <ziyankarmali786@gmail.com>
2020-05-15 17:54:40 +0530

    Added all required routes

--- src/controllers/userControls.js
+++ src/controllers/userControls.js
@@ -1,5 +1,10 @@
 const { admin, database } = require('../utils/firebase')
 const chalk = require('chalk')
+const FieldValue = require('firebase-admin').FieldValue;
+
+var design = ['mobile app', 'website', 'tv interface', 'app interface'];
+var forA = ['a restaurant in Brazil', 'a watch shop in Germany', 'a clothing shop in Dharavi', 'an ice cream parlour in Hawaii', 'a juice shop in Iceland', 'a tree in a desert', 'a book shop', 'bottle in San Jose', 'a devil in Heaven', 'a chair in IEEE']
+var toHelp = ['a man in Brazil', 'to flash a torch', 'get groceries', 'animals get happy', 'redo an ML model', 'trains go faster', 'Pablo Escobar in Katpadi', 'relax your mind', 'too lazy to think', 'a chair in IEEE']
 
 const createUser = (user) => {
     return new Promise((resolve, reject) => {
@@ -53,6 +58,36 @@ const checkUserUid = (uid) => {
     })
 }
 
+const checkUserObject = (uid, resp) => {
+    return new Promise(async (resolve, reject) => {
+        console.log("Entered checkUserObject")
+        const userRef = await database.collection('Users').doc(uid)
+        console.log("wait")
+        userRef.get()
+            .then((docSnapshot) => {
+                console.log("got docSnapshot")
+                if (docSnapshot.exists) {
+                    console.log(docSnapshot.exists)
+                    resolve({
+                        statusCode: 200,
+                        payload: {
+                            msg: "User Checked",
+                            responce: resp
+                        }
+                    })
+                }
+            })
+            .catch((err) => {
+                console.log(chalk.red("User uid un-verified from database!"))
+                reject({
+                    statusCode: 400,
+                    error: err.message,
+                    message: "Unauthorised"
+                })
+            })
+    })
+}
+
 const getUserInfo = (uid) => {
     return new Promise((resolve, reject) => {
         console.log(chalk.yellow("Getting user info..."))
@@ -76,8 +111,124 @@ const getUserInfo = (uid) => {
     })
 }
 
+const createProblemStatement = (uid, isLock1, isLock2, isLock3) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            console.log(chalk.yellow("Getting new problem statement..."))
+            const userRef = await database.collection('Users').doc(uid).get().then((doc)=> {
+                count = doc.data().count
+            })
+            if(count > 0) {
+                if(isLock1===false) {
+                    newDesign = design[Math.floor(Math.random() * 4)]
+                } else {
+                    newDesign = "Locked"
+                }
+                if(isLock2===false) {
+                    newTo = forA[Math.floor(Math.random() * 10)]
+                } else {
+                    newTo = "Locked"
+                }
+                if(isLock3===false) {
+                    newToHelp = toHelp[Math.floor(Math.random() * 5)]
+                } else {
+                    newToHelp = "Locked"
+                }
+                resolve({
+                    payload: {
+                        newDesign: newDesign,
+                        newTo: newTo,
+                        newToHelp: newToHelp,
+                        count: count
+                    }
+                })
+                const userRef = await database.collection('Users').doc(uid).update({count : admin.firestore.FieldValue.increment(-1) })
+            } else {
+                resolve({
+                    payload: {
+                        count: "0",
+                        message: "No more tries left"
+                    }
+                })
+            }
+        } catch(e) {
+            reject({
+                payload: {
+                    message: "Something went wrong with generating problem statement. Please try again"
+                }
+            })
+        }
+    })
+}
+
+const lockProblem = (uid, value1, value2, value3) => {
+    return new Promise(async(resolve, reject) => {
+        task = "Design a "+value1+" for "+value2+" to help "+value3;
+        try {
+            const userRef = await database.collection('Users').doc(uid).update({task:task})
+            resolve({
+                payload: {
+                    message: "Successfully stored task"
+                }
+            })
+        } catch(e) {
+            console.log(e);
+            reject({
+                payload: {
+                    message: "Not saved task. Please try again"
+                }
+            })
+        }
+        
+    })
+}
+
+const submitLink = (uid, link) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            const userRef = await database.collection('Users').doc(uid).update({link1:link});
+            resolve({
+                payload: {
+                    message: "Successfully stored link"
+                }
+            })
+        } catch(e) {
+            reject({
+                payload: {
+                    message: "Not saved link. Please try again"
+                }
+            })
+        }
+    })
+}
+
+const promises = []
+const addCount = () => {
+    return new Promise(async(resolve, reject) => {
+        const userRef = await database.collection('Users').get()
+        .then(snapshot => {
+            const promises = []
+            snapshot.forEach(doc => {
+                promises.push(doc.ref.set({
+                    count:5
+                }))
+            })
+        })
+        await Promise.all(promises)
+        resolve({
+            payload: {
+                "done":"done"
+            }
+        })
+    })
+}
+
 module.exports = {
     createUser,
     checkUserUid,
-    getUserInfo
+    getUserInfo,
+    checkUserObject,
+    createProblemStatement,
+    lockProblem,
+    addCount
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express")();
 const userCreate = require('../middlewares/userCreateMiddleware');
+const userAuth = require('../middlewares/userAuth');
 const userControls = require('../controllers/userControls');
 
 router.post('/create',[userCreate], (req, res) => {
@@ -7,5 +8,35 @@ router.post('/create',[userCreate], (req, res) => {
         .then(resp => res.status(200).send(resp))
         .catch(err => res.status(400).send(err))
 })
+
+router.post('/generateProblem',[userAuth], (req, res) => { //[userAuth],
+    userControls.createProblemStatement(
+        req.body.uid,
+        req.body.isLock1,
+        req.body.isLock2,
+        req.body.isLock3
+    )
+        .then(resp => res.status(200).send(resp))
+        .catch(err => res.status(400).send(err))
+})
+
+router.post('/lockProblem',[userAuth], (req, res) => {
+    userControls.lockProblem(
+        req.body.uid,
+        req.body.value1,
+        req.body.value2,
+        req.body.value3
+    )
+        .then(resp => res.status(200).send(resp))
+        .catch(err => res.status(400).send(err))
+})
+
+router.get('/addCount', (req,res) => {
+    userControls.addCount()
+    .then(resp => res.status(200).send(resp))
+    .catch(err => res.status(400).send(err))
+})
+
+
 
 module.exports = router;
